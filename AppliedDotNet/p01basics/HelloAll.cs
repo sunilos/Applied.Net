ba79f66ec8639a5e7dfa6724cd3ce8b8cc1a2c10
shubhkumawat1414 <shubham9294525058@gmail.com>
2022-12-12 12:18:00 +0530

    .Net

---
+++ AppliedDotNet/p01basics/HelloAll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliedDotNet.p01basics
+{
+    internal class HelloAll
+    {
+        public static void RunTime(String[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(i + " = Hello " + args[i]);
+            }
+        }
+    }
+}
