efa307445f5cff341512930e2871fc81438bcfdf
shubhkumawat1414 <106065587+shubhkumawat1414@users.noreply.github.com>
2022-12-12 12:59:36 +0530

    Update HelloAll.cs

--- AppliedDotNet/p01basics/HelloAll.cs
+++ AppliedDotNet/p01basics/HelloAll.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AppliedDotNet.p01basics
 {
-    internal class HelloAll
+    public class HelloAll
     {
         public static void RunTime(String[] args)
         {
