d62667339491ef17ab7fc936b4495b5464964183
shubhkumawat1414 <shubham9294525058@gmail.com>
2022-12-12 12:04:00 +0530

    .NET

---
+++ AppliedDotNet/src/com/AppliedDotNet/AppliedDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using AppliedDotNet.p01basics;
+
+namespace AppliedDotNet
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HelloAll.Test();
+        }
+    }
+}
---
+++ AppliedDotNet/src/com/AppliedDotNet/AppliedDotNet/p01basics/HelloAll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliedDotNet.p01basics
+{
+    public class HelloAll
+    {
+        public static void Test()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i + " = Hello ");
+            }
+        }
+    }
+}
